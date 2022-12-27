5a8b620cc56195fa04cd57a80ad6c84e35e2c66a
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2022-12-27 15:14:57 +0600

    up

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask,request,jsonify
 import pandas as pd
+import os
+import json
+
+from flask import send_file
+
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -9,10 +15,7 @@ def index():
     "duration": [50, 40, 45]
     }
     df = pd.DataFrame(data)
-
-    # df.to_csv('made.csv')
-    # df=pd.read_csv('liver.csv')
-    return df.to_html()
+    return df.to_html()     # to_string(),to_dict(),to_json(0)
 
 book_list=[
     {
@@ -25,3 +28,19 @@ book_list=[
 @app.route('/books',methods=['GET'])
 def books():
     return jsonify(book_list)
+
+
+@app.route('/csv-read')
+def data():
+    df=pd.read_csv(os.path.abspath(os.getcwd())+"/made.csv",encoding='windows-1252')  # also 1254
+    return df.to_json()
+
+
+@app.route('/csv-download') # this is a job for GET, not POST
+def plot_csv():
+    return send_file(
+        'made.csv',
+        mimetype='text/csv',
+        download_name='name.csv',
+        as_attachment=True
+    )
